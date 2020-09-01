bc9cda621cf8bd5dcc93d0e0827219035e72bbc7
Mark Lavi <mlavi@users.noreply.github.com>
2020-09-01 08:14:13 -0500

    Refactored

--- optional/SetHostname.sh
+++ optional/SetHostname.sh
@@ -4,6 +4,7 @@ set -ex
 if [[ -n '@@{name}@@' ]]; then
   _hostname='@@{name}@@'
 else
+  _hostname='web@@{calm_array_index}@@'
   _hostname='HAProxy' # system_release.txt?
 fi
 cat "${_hostname}" | sudo tee /etc/hostname
